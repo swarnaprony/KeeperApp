a4d05663f9303d716502e76e937ad6e6ac0e1ab4
Swarna Sarker <swarnaprony81@gmail.com>
2022-09-15 16:29:46 +0200

    First Commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+    return <div className="note">
+        <h1>Here the title will go</h1>
+        <p>Content</p>
+    </div>
+}
+
+export default Note;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+<link rel="stylesheet" href="styles.css"></link>
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
