9b771b92f92f2dfdaa98555337d99667b4109c52
Swarna Sarker <swarnaprony81@gmail.com>
2022-09-22 22:48:15 +0200

    Update App.jsx to add add note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
@@ -6,17 +6,79 @@ import notes from "./notes";
 
 
 function App() {
-    return (<div>
-        <Header/>
-        <div>
-            {notes.map(note => (
-                <Note
-                    key={note.key}
-                    title={note.title}
-                    content={note.content}
-                />)
-                )}
+
+    const [newNotes, setNewNotes] = useState([...notes]);
+    const [recentNote, setRecentNote] = useState([{
+        title:"",
+        content: ""}]);
+
+    function handleChange(event) {
+        console.log("Changed")
+
+       const {value, name} = event.target;
+
+       setRecentNote((prevValue) => {
+           if (name === "title") {
+               return {
+               title: value,
+               content: prevValue.content
+               };
+           } else if (name === "content") {
+               return{
+                title: prevValue.title,
+                content: value
+               };
+                
+           }
+       });
+    }
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Submitted")
+        setNewNotes((prevNote) => {
+            return [...prevNote, recentNote]
+        })
+
+
+    }
+
+    return (
+    <div>
+        <Header/>  
+        <form onSubmit={handleSubmit}>
+            <h4>Add Title</h4>
+            <div>
+            <input 
+                onChange={handleChange} 
+                type="text" 
+                placeholder="title" 
+                name="title"
+            /><br></br>
+            <input 
+                onChange={handleChange} 
+                type="text" 
+                placeholder="content" 
+                name="content" 
+            /><br></br>
+           
         </div>
+            <button type="submit">Submit</button>
+        </form>                 
+        
+        <div>
+        {newNotes.map(note => (
+            <Note
+                key={note.key}
+                title={note.title}
+                content={note.content}
+        />)
+
+        )}
+
+    </div>
+        
         <Footer />
     </div>)
 }
